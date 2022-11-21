1cfcc951789d9662c34352a7bc1efb40a4d6806f
MihaiCovasan <92455723+MihaiCovasan@users.noreply.github.com>
2022-11-21 09:17:53 +0200

    Commit 1

---
+++ Guess the number/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Random rand = new Random();
+int guess = 0;
+int minNumber, maxNumber;
+Console.WriteLine("Dati minNumber: ");
+minNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Dati maxNumber: ");
+maxNumber = Convert.ToInt32(Console.ReadLine());
+int numar = rand.Next(minNumber, maxNumber);
+
+while (guess != numar)
+{
+    Console.WriteLine("Dati guess: ");
+    guess = Convert.ToInt32(Console.ReadLine());
+    if (guess > numar)
+    {
+        Console.WriteLine("Valoarea {0} introdusa este prea mare ", guess);
+    }
+    else if(guess < numar)
+    {
+        Console.WriteLine("Valoarea {0} introdusa este prea mica", guess);
+    }
+
+    
+}
+
+Console.WriteLine("Valoarea {0} introdusa este corecta", guess);
+
+
